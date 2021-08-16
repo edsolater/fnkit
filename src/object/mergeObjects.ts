c8b884dd5f7af9e504211ce7b97223127881adfb
edsolater <edsolater@qq.com>
2021-08-17 01:01:13 +0800

    new mergeObjects

--- src/object/mergeObjects.ts
+++ src/object/mergeObjects.ts
@@ -1,11 +1,15 @@
+import objectFilter from '../object/objectFilter'
+
 /**
  * 合并多个对象（会返回新对象）(浅复制)
  */
+function mergeObjects<T>(obj1: T): T
 function mergeObjects<T, U>(obj1: T, obj2: U): T & U
 function mergeObjects<T, U, V>(obj1: T, obj2: U, obj3: V): T & U & V
 function mergeObjects<T, U, V, W>(obj1: T, obj2: U, obj3: V, obj4: W): T & U & V & W
-function mergeObjects<T extends object >(...objs: T[]): T
+function mergeObjects<T extends object>(...objs: T[]): T
 function mergeObjects<T extends object>(...objs: T[]) {
-  return Object.assign({}, ...objs)
+  const result = Object.assign({}, ...objs)
+  return objectFilter(result, ([, value]) => value !== undefined)
 }
 export default mergeObjects
