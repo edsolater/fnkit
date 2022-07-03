71badf77aeeb80f36e6099b85d816cbd569ad857
edsolater <edsolater@qq.com>
2022-07-03 19:16:50 +0800

    ✨ add `listToMap()`

--- src/collectionMethods/groupBy.ts
+++ src/collectionMethods/groupBy.ts
@@ -8,18 +8,18 @@ import { toEntries } from './entries'
  * @example
  * console.log(groupBy([1, 2, 4, 0], (v) => v % 2 ? 'even' : 'odd' )) // { odd: [1, 0], even: [2, 4] }
  */
-export function groupBy<T extends AnyArr, GroupName extends string>(
+export function groupBy<T extends AnyArr, GroupName extends string | number | undefined>(
   arr: T,
   predicate: (item: T[number], index: number, arr: T) => GroupName
-): Record<GroupName, Partial<T> | undefined>
-export function groupBy<K, V, GroupName extends string>(
+): Record<NonNullable<GroupName>, Partial<T> | undefined>
+export function groupBy<K, V, GroupName extends string | number | undefined>(
   collection: Map<K, V>,
   predicate: (value: V, key: K, map: Map<K, V>) => GroupName
-): Record<GroupName, Map<K, V> | undefined>
-export function groupBy<O extends AnyObj, GroupName extends string>(
+): Record<NonNullable<GroupName>, Map<K, V> | undefined>
+export function groupBy<O extends AnyObj, GroupName extends string | number | undefined>(
   obj: O,
   predicate: (value: SValueof<O>, key: SKeyof<O>, obj: O) => GroupName
-): Record<GroupName, Partial<O> | undefined>
+): Record<NonNullable<GroupName>, Partial<O> | undefined>
 export function groupBy(collection, predicate) {
   return isArray(collection)
     ? jsArrayGroupBy(collection, predicate)
@@ -28,10 +28,10 @@ export function groupBy(collection, predicate) {
     : jsObjectGroupBy(collection, predicate)
 }
 
-function jsArrayGroupBy<T extends AnyArr, GroupName extends string>(
+function jsArrayGroupBy<T extends AnyArr, GroupName extends string | number | undefined>(
   arr: T,
   predicate: (item: T[number], index: number, arr: T) => GroupName
-): Record<GroupName, Partial<T> | undefined> {
+): Record<NonNullable<GroupName>, Partial<T> | undefined> {
   return arr.reduce((acc, item, idx) => {
     const groupName = predicate(item, idx, arr)
     if (groupName) acc[groupName] = [...(acc[groupName] ?? []), item]
@@ -39,20 +39,20 @@ function jsArrayGroupBy<T extends AnyArr, GroupName extends string>(
   }, {})
 }
 
-function jsObjectGroupBy<O extends AnyObj, GroupName extends string>(
+function jsObjectGroupBy<O extends AnyObj, GroupName extends string | number | undefined>(
   obj: O,
   predicate: (value: SValueof<O>, key: SKeyof<O>, obj: O) => GroupName
-): Record<GroupName, Partial<O> | undefined> {
+): Record<NonNullable<GroupName>, Partial<O> | undefined> {
   const entries = toEntries(obj)
   const groupedEntries = jsArrayGroupBy(entries, ([k, v]) => predicate(v, k as SKeyof<O>, obj))
   //@ts-expect-error inner core , no need to worry
   return map(groupedEntries, (entries) => (entries ? Object.fromEntries(shakeNil(entries)) : undefined))
 }
 
-function jsMapGroupBy<K, V, GroupName extends string>(
+function jsMapGroupBy<K, V, GroupName extends string | number | undefined>(
   collection: Map<K, V>,
   predicate: (value: V, key: K, map: Map<K, V>) => GroupName
-): Record<GroupName, Map<K, V> | undefined> {
+): Record<NonNullable<GroupName>, Map<K, V> | undefined> {
   const entries = toEntries(collection)
   const groupedEntries = jsArrayGroupBy(entries, ([k, v]) => predicate(v, k, collection))
   //@ts-expect-error inner core , no need to worry
---
+++ src/listToMap.ts
@@ -0,0 +1,25 @@
+/**
+ * list of object item to object of item
+ * @example
+ * listToMap([{id:'xxx', name:'hello'}, {id:'yy', name:'world'}], i => i.id) => {xxx: {id:'xxx', name:'hello'}, yy: {id:'yy', name:'world'}}
+ */
+export function listToMap<T, S extends string, V = T>(
+  source: T[],
+  getKey: (item: T, index: number) => S,
+  getValue?: (item: T, index: number) => V
+): Record<S, V> {
+  // @ts-expect-error force
+  return Object.fromEntries(source.map((item, idx) => [getKey(item, idx), getValue ? getValue(item, idx) : item]))
+}
+
+/**
+ * @returns a js Map object instead of a plain object
+ */
+export function listToJSMap<T, S, V = T>(
+  source: T[],
+  getKey: (item: T, index: number) => S,
+  getValue?: (item: T, index: number) => V
+): Map<S, V> {
+  // @ts-expect-error force
+  return new Map(source.map((item, idx) => [getKey(item, idx), getValue ? getValue(item, idx) : item]))
+}
