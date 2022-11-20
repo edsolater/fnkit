b962b3b421dd656934522f1d5e4fc284d6b3915a
edsolater <edsolater@qq.com>
2022-11-20 23:47:14 +0800

    🏷️ improve `omit()`

--- src/collectionMethods/arrayEnhanceMethods.ts
+++ src/collectionMethods/arrayEnhanceMethods.ts
@@ -10,18 +10,8 @@ import filter from './filter'
  * @example
  * console.log(omit({ a: 1, b: true }, ['a'])) //=> { b: true }
  */
-export function omit<T extends Collection, U extends string>(
-  collection: T,
-  propNameList: MayArray<string>
-): Partial<T>
-export function omit<T extends Collection, U extends keyof T>(
-  collection: T,
-  propNameList: MayArray<U>
-): Omit<T, U> {
-  return filter(
-    collection,
-    (_value, key) => !flap(propNameList).includes(key as any)
-  ) as unknown as Omit<T, U>
+export function omit<T extends Collection, U extends keyof T>(collection: T, propNameList: MayArray<U>): Omit<T, U> {
+  return filter(collection, (_value, key) => !flap(propNameList).includes(key as any)) as unknown as Omit<T, U>
 }
 
 /**
@@ -31,14 +21,8 @@ export function omit<T extends Collection, U extends keyof T>(
  * @example
  * pick({ a: 1, b: 2 }, ['a']) // { a: 1 }
  */
-export function pick<T extends Collection, U extends keyof T>(
-  collection: T,
-  propNameList: U[]
-): Pick<T, U> {
-  return filter(collection, (_value, key) => propNameList.includes(key as any)) as unknown as Pick<
-    T,
-    U
-  >
+export function pick<T extends Collection, U extends keyof T>(collection: T, propNameList: U[]): Pick<T, U> {
+  return filter(collection, (_value, key) => propNameList.includes(key as any)) as unknown as Pick<T, U>
 }
 
 export function getKeys<T extends Collection>(collection: T): GetCollectionKey<T>[] {
