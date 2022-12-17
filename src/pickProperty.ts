fb8d718df9d01757d056fad4222da7af24fef2a8
edsolater <edsolater@qq.com>
2022-12-17 13:45:11 +0800

    🚧 create `loopSelf`

--- src/pickProperty.ts
+++ src/pickProperty.ts
@@ -3,3 +3,7 @@ import { AnyObj } from './typings'
 export function pickProperty<T extends AnyObj, P extends keyof T = keyof T>(property: P): (target: T) => T[P] {
   return (target) => target[property]
 }
+/** FP util function. do nothing */
+export function loopSelf<T>(i: T) {
+  return i
+}
