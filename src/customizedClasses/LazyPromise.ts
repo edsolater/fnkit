4906584cbd58924d46021451dde194998b7c54b9
zhanggongying <edsolater@qq.com>
2023-02-06 12:33:54 +0800

    🚧 improve `LazyPromise`

--- src/customizedClasses/LazyPromise.ts
+++ src/customizedClasses/LazyPromise.ts
@@ -1,3 +1,4 @@
+import { isFunction } from '../dataType'
 import { shrinkToValue } from '../wrapper'
 
 /**
@@ -17,26 +18,47 @@ import { shrinkToValue } from '../wrapper'
  *   return LazyPromise.resolve(() => 2)
  * })
  */
-export class LazyPromise<T> extends Promise<T> {
-  #executor: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void
-  constructor(executor: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void) {
-    super((resolve) => {
-      resolve(undefined as T)
+export class LazyPromise<T = undefined> extends Promise<T> {
+  #executor?: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void
+  #innerPromise?: Promise<T>
+  #loaded = false
+
+  constructor(
+    executor?: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void,
+    options?: {
+      initLoad?: boolean
+    }
+  ) {
+    super((resolve, reject) => {
+      resolve(undefined as T) // not elegent, but works
     })
     this.#executor = executor
+    if (options?.initLoad) this.load()
   }
-  
+
   static resolve<T>(value?: T | PromiseLike<T> | (() => T | PromiseLike<T>)) {
-    return new LazyPromise<T>((resolve) => {
-      resolve(shrinkToValue(value))
-    })
+    return new LazyPromise<T>(
+      (resolve) => {
+        resolve(shrinkToValue(value))
+      },
+      { initLoad: !isFunction(value) }
+    )
+  }
+
+  /** by default, executor is not loaded. but manually call this will manually load promise
+   * need #executor to be fullfilled
+   */
+  load() {
+    this.#innerPromise = this.#executor ? new Promise(this.#executor) : Promise.resolve(undefined as T)
+    this.#loaded = true
   }
 
   then<TResult1 = T, TResult2 = never>(
     onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null,
     onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null
   ): Promise<TResult1 | TResult2> {
-    return new Promise(this.#executor).then(onfulfilled, onrejected)
+    if (!this.#loaded) this.load()
+    return this.#innerPromise!.then(onfulfilled, onrejected)
   }
 
   /**
@@ -47,7 +69,8 @@ export class LazyPromise<T> extends Promise<T> {
   catch<TResult = never>(
     onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null
   ): Promise<T | TResult> {
-    return new Promise(this.#executor).catch(onrejected)
+    if (!this.#loaded) this.load()
+    return this.#innerPromise!.catch(onrejected)
   }
 
   /**
@@ -56,7 +79,8 @@ export class LazyPromise<T> extends Promise<T> {
    * @returns A Promise for the completion of the callback.
    */
   finally(onfinally?: (() => void) | null | undefined): Promise<T> {
-    return new Promise(this.#executor).finally(onfinally)
+    if (!this.#loaded) this.load()
+    return this.#innerPromise!.finally(onfinally)
   }
 
   get [Symbol.toStringTag]() {
