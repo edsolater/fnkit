9cd19f55954069dbfcba8ce0b05ebb739036cab8
zhanggongying <edsolater@qq.com>
2023-02-16 15:41:47 +0800

    🚧 `shrinkFn`

--- src/wrapper/index.ts
+++ src/wrapper/index.ts
@@ -1,2 +1,4 @@
 export { default as shrinkToValue } from './shrinkToValue'
+export * from './shrinkToValue'
 export { default as wrapePromise } from './wrapPromise'
+export * from './wrapPromise'
--- src/wrapper/shrinkToValue.ts
+++ src/wrapper/shrinkToValue.ts
@@ -17,12 +17,19 @@ export const flatWithFn = <T extends MayArray<MayFn<any>>>(
  * @param mayValue maybe a function that will return targetValue
  * @param params the parameters that will be passed in mayValue(if it's function)
  * @returns a pure value which can't be a function
+ * @deprecated use {@link shrinkFn} instead
  */
-
-export default function shrinkToValue<T extends MayFn<any>>(
-  mayValue: T,
-  params?: MayParameters<T>
-): MayReturn<T> {
+export default function shrinkToValue<T extends MayFn<any>>(mayValue: T, params?: MayParameters<T>): MayReturn<T> {
+  return isFunction(mayValue) ? mayValue(...(params ?? [])) : mayValue
+}
+/**
+ * get value from input, if input is a function. it will ve invoked
+ *
+ * @param mayValue maybe a function that will return targetValue
+ * @param params the parameters that will be passed in mayValue(if it's function)
+ * @returns a pure value which can't be a function
+ */
+export function shrinkFn<T extends MayFn<any>>(mayValue: T, params?: MayParameters<T>): MayReturn<T> {
   return isFunction(mayValue) ? mayValue(...(params ?? [])) : mayValue
 }
 
