6094ad80d8ae6f0c8c72f63fb65b1385a78d948d
edsoalter <edsolater@qq.com>
2023-03-04 00:24:26 +0800

    🚧 improve `mergeFunction()`

--- src/mergeFunctions.ts
+++ src/mergeFunctions.ts
@@ -7,10 +7,22 @@ import { AnyFn } from './typings/constants'
  * const multi = (a: number) => 3 + a
  * const c = mergeFunction(add, multi) // (a: number, b: number) => {add(a, b); multi(a, b)}
  */
-export function mergeFunction<T extends AnyFn | undefined>(
-  ...fns: T[]
-): (...params: T extends AnyFn ? Parameters<T> : any) => void {
-  return (...params) => {
-    fns.forEach((fn) => fn?.(...(params as any)))
-  }
+export function mergeFunction<T extends AnyFn>(...fns: [T]): (...params: Parameters<T>) => [ReturnType<T>]
+export function mergeFunction<T extends AnyFn, U extends (...args: Parameters<T>) => any>(
+  ...fns: [T, U]
+): (...params: Parameters<T>) => [ReturnType<T>, ReturnType<U>]
+export function mergeFunction<
+  T extends AnyFn,
+  U extends (...args: Parameters<T>) => any,
+  V extends (...args: Parameters<T>) => any
+>(...fns: [T, U, V]): (...params: Parameters<T>) => [ReturnType<T>, ReturnType<U>, ReturnType<V>]
+export function mergeFunction<
+  T extends AnyFn,
+  U extends (...args: Parameters<T>) => any,
+  V extends (...args: Parameters<T>) => any,
+  W extends (...args: Parameters<T>) => any
+>(...fns: [T, U, V, W]): (...params: Parameters<T>) => [ReturnType<T>, ReturnType<U>, ReturnType<V>, ReturnType<W>]
+export function mergeFunction<T extends AnyFn>(...fns: T[]): (...params: Parameters<T>) => ReturnType<T>[]
+export function mergeFunction<T extends AnyFn>(...fns: T[]): (...params: Parameters<T>) => ReturnType<T>[] {
+  return (...params) => fns.map((fn) => fn?.(...params))
 }
