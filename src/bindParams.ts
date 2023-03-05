0746dac06cbcb1d41ff67f6423283fbca287a78d
edsolater <edsolater@qq.com>
2023-03-05 22:27:23 +0800

    🚧`changeFnReturnValue()`

--- src/bindParams.ts
+++ src/bindParams.ts
@@ -3,11 +3,30 @@ import { AnyFn } from '.'
 /**
  * solid params and return a new function
  * but, it will lost correct type
+ * @example
+ * const newFn = bindParams(connectWallet, [, config])
+ * const v = newFn(wallet)
+ * const sameAs = connectWallet(wallet, config)
  */
-export function bindParams<T extends AnyFn, Pa extends Partial<Parameters<T>> & unknown[]>(
+export function bindParams<T extends AnyFn>(
   fn: T,
-  params: [...Pa],
-  thisArg: any = undefined
+  params: Partial<Parameters<T>>
 ) {
-  return fn.bind(thisArg, params) as (...arg: Partial<Parameters<T>> & unknown[]) => ReturnType<T>
+  return ((...args: any[]) => {
+    const newParams = [...args]
+    Object.entries(params).forEach(([idx, v]) => {
+      newParams[idx] = v
+    })
+    return fn(...newParams)
+  }) as //@ts-expect-error don't know how to type
+  (...arg: Partial<Parameters<T>>) => ReturnType<T>
 }
+
+type MakeItemsOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+function add(a: number, b: number, c: string) {
+  return a + b
+}
+const fn = bindParams(add, [,,'c'])
+fn(1, 2)
+
+type C = [qr: number, sdf: number, ehl: string]
--- src/changeFnReturnValue.ts
+++
@@ -1,9 +0,0 @@
-import { AnyFn } from './typings/constants';
-
-
-export function changeFnReturnValue<T extends AnyFn, U>(
-  fn: T,
-  changer: (returnValue: ReturnType<T>) => U
-): (...params: Parameters<T>) => U {
-  return (...params) => changer(fn(...params));
-}
