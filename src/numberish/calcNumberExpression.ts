decf9332e39985c4ca2efc03f424b4f84ce41b82
edsoalter <edsolater@qq.com>
2023-05-03 00:22:19 +0800

    🚧 prettifyNumberishAtom decimal 10

--- src/numberish/calcNumberExpression.ts
+++ src/numberish/calcNumberExpression.ts
@@ -2,7 +2,6 @@
  * @see https://m.xp.cn/b.php/107696.html
  */
 
-import { isNull } from '../dataType'
 import { Numberish, NumberishAtom, NumberishAtomRaw } from '../typings'
 import { isNumberishAtom, isNumberishAtomRaw, toNumberishAtom } from './numberishAtom'
 import { add, div, minus, mul } from './operations'
@@ -159,4 +158,89 @@ function toExpression(n: Numberish) {
   }
 }
 
-console.log(toExpression(parseRPN(toRPN('1*(-3)/2'))))
+function padTailZero(n: string | bigint | number, zeroLength?: number) {
+  if (!zeroLength) return String(n)
+  return zeroLength > 0 ? String(n) + ''.padEnd(zeroLength, '0') : String(n).slice(0, zeroLength)
+}
+function getTailZeroLength(n: string | bigint | number) {
+  return String(n).match(/0+$/)?.[0].length ?? 0
+}
+
+/**
+ * @example
+ * prettifyNumberishAtom({ numerator: 100n, denominator: 133000n, decimal: 1}) //=> { numerator: 100n, denominator: 133000n}
+ */
+function prettifyNumberishAtomWithDecimal(n: NumberishAtomRaw): { numerator: bigint; denominator: bigint } {
+  if (!n.decimal) return { numerator: n.numerator, denominator: n.denominator ?? 1n }
+
+  let finalNumerator = n.numerator
+  let finalDenominator = n.denominator ?? 1n
+  let finalDecimal = n.decimal ?? 0
+
+  const numberatorZeroLength = getTailZeroLength(finalNumerator)
+  if (finalDecimal && finalDecimal > 0) {
+    if (numberatorZeroLength >= finalDecimal) {
+      finalNumerator = BigInt(padTailZero(finalNumerator, -finalDecimal))
+      finalDecimal = 0
+    } else {
+      if (numberatorZeroLength) {
+        finalNumerator = BigInt(padTailZero(finalNumerator, -numberatorZeroLength))
+      }
+      finalDenominator = BigInt(padTailZero(finalDenominator, finalDecimal - numberatorZeroLength))
+      finalDecimal = 0
+    }
+  }
+
+  const denominatorZeroLength = getTailZeroLength(finalDenominator)
+  if (finalDecimal && finalDecimal < 0) {
+    if (denominatorZeroLength >= -finalDecimal) {
+      finalDenominator = BigInt(padTailZero(finalDenominator, finalDecimal))
+      finalDecimal = 0
+    } else {
+      if (denominatorZeroLength) {
+        finalDenominator = BigInt(padTailZero(finalDenominator, -denominatorZeroLength))
+      }
+      finalNumerator = BigInt(padTailZero(finalNumerator, -finalDecimal - denominatorZeroLength))
+      finalDecimal = 0
+    }
+  }
+  return {
+    numerator: finalNumerator,
+    denominator: finalDenominator
+  }
+}
+
+/**
+ * @example
+ * prettifyNumberishAtom({ numerator: 100n, denominator: 133000n}) //=> { numerator: 1n, denominator: 1330n}
+ */
+function prettifyNumberishAtomWith10(n: { numerator: bigint; denominator: bigint }): {
+  numerator: bigint
+  denominator: bigint
+} {
+  const canNumeratorMod10RestLength = String(n.numerator).endsWith('0')
+    ? String(n.numerator).match(/0+$/)?.[0].length
+    : 0
+  const canDenominatorMod10RestLength = String(n.denominator).endsWith('0')
+    ? String(n.denominator).match(/0+$/)?.[0].length
+    : 0
+  if (canNumeratorMod10RestLength && canDenominatorMod10RestLength) {
+    const restLength = Math.min(canNumeratorMod10RestLength, canDenominatorMod10RestLength)
+    return {
+      numerator: BigInt(padTailZero(n.numerator, -restLength)),
+      denominator: BigInt(padTailZero(n.denominator, -restLength))
+    }
+  }
+  return n
+}
+
+function prettifyNumberishAtom(n: NumberishAtomRaw): NumberishAtom {
+  //@ts-expect-error Temp for DEV
+  return prettifyNumberishAtomWith10(prettifyNumberishAtomWithDecimal(n))
+}
+console.log(prettifyNumberishAtom({ numerator: 10n, denominator: 133n, decimal: 2 }))
+console.log(prettifyNumberishAtom({ numerator: 100n, denominator: 133000n, decimal: 1 }))
+
+console.log(prettifyNumberishAtom({ numerator: 10n, denominator: 13300n, decimal: -2 }))
+console.log(prettifyNumberishAtom({ numerator: 100n, denominator: 133n, decimal: -1 }))
+console.log(toExpression(parseRPN(toRPN('1*(-302)/2'))))
