3ceec82061c885637e0cde28485b084d2a7021b2
edsoalter <edsolater@qq.com>
2023-08-06 01:13:13 +0800

    ⚡faster mergeObjectsWithConfigs

--- src/mergeObjectsWithConfigs.ts
+++ src/mergeObjectsWithConfigs.ts
@@ -1,5 +1,4 @@
 import { unifyItem } from './collectionMethods'
-import { createObjectByGetters } from './createObjectByGetters';
 
 /**
  * merge without access, you can config transformer for detail control
@@ -12,9 +11,25 @@ export function mergeObjectsWithConfigs<T extends object>(
 ): T {
   if (objs.length === 0) return {} as T
   if (objs.length === 1) return objs[0]!
-  return createObjectByGetters(
-    Object.fromEntries(getObjKey(objs).map((key) => [key, () => getValue(objs, key, transformer)]))
-  ) as T
+  return new Proxy(createEmptyObjectByOlds(objs), {
+    get(target, key, receiver) {
+      return getValue(objs, key, transformer)
+    }
+  }) as T
+}
+
+export function mergeObjectCoverly<T extends object>(objs: T[]): T {
+  if (objs.length === 0) return {} as T
+  if (objs.length === 1) return objs[0]!
+  return new Proxy(createEmptyObjectByOlds(objs), {
+    get(target, key, receiver) {
+      return getValue(objs, key, ({ valueA, valueB }) => valueB ?? valueA)
+    }
+  }) as T
+}
+
+function createEmptyObjectByOlds(objs: object[]) {
+  return objs.reduce((acc, cur) => Object.assign(acc, Object.getOwnPropertyDescriptors(cur)), {})
 }
 
 function getValue<T extends object>(
@@ -22,10 +37,13 @@ function getValue<T extends object>(
   key: string | symbol,
   valueMatchRule: (payloads: { key: string | symbol; valueA: any; valueB: any }) => any
 ) {
-  return objs.map((o) => o[key]).reduce((valueA, valueB) => valueMatchRule({ key, valueA, valueB }), undefined)
+  return objs.reduce((valueA, obj) => {
+    const valueB = obj[key]
+    const mergedValue = valueA != null && valueB !== null ? valueMatchRule({ key, valueA, valueB }) : valueB ?? valueA
+    return mergedValue
+  }, undefined)
 }
 
 function getObjKey<T extends object>(objs: T[]) {
   return unifyItem(objs.flatMap((obj) => Reflect.ownKeys(obj)))
 }
-
