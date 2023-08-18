f1856b394a2a4af362a5575ea52bc98bf9e3d27c
edsoalter <edsolater@qq.com>
2023-08-18 10:39:54 +0800

    ✨ clear for eventCenter

--- src/customizedClasses/EventCenter.ts
+++ src/customizedClasses/EventCenter.ts
@@ -1,4 +1,3 @@
-import { uncapitalize } from '../changeCase'
 import { map } from '../collectionMethods'
 import { AnyFn } from '../typings'
 import { createSubscription, Subscription } from './Subscription'
@@ -18,23 +17,38 @@ export type EventCenterOptions = {
 }
 
 export type EventCenter<T extends EventConfig> = {
+  /** core for event provider */
   emit<N extends keyof T>(eventName: N, parameters: Parameters<T[N]>): void
+
+  /** same as {@link EventCenter['on']} but will regist multi listeners at once */
   registEvents<U extends Partial<T>>(subscriptionFns: U, options?: EventCenterOptions): { [P in keyof U]: Subscription }
+
+  /** core for event consumer */
+  /**
+   * create a factory
+   */
+  on<N extends keyof T>(
+    eventName: N
+  ): (subscriptionFn: (...params: Parameters<T[N]>) => void, options?: EventCenterOptions) => Subscription
+  /**
+   * subscribe
+   */
   on<N extends keyof T>(
     eventName: N,
     subscriptionFn: (...params: Parameters<T[N]>) => void,
     options?: EventCenterOptions
   ): Subscription
-} & {
-  [P in keyof T as `on${Capitalize<P & string>}`]: (
-    subscriptionFn: (...params: Parameters<T[P]>) => void,
-    options?: EventCenterOptions
-  ) => Subscription
+
+  /** clear all registed events for specified event */
+  clearAll(): void
+
+  /** clear registed for specified event */
+  clear(eventName: keyof T): void
 }
 
 /**
  * @example
- * 
+ *
  * //step 1: regist events (3 ways)
  *  // client side
  * cc.registEvents({
@@ -57,19 +71,13 @@ export type EventCenter<T extends EventConfig> = {
  * cc.emit('change', [{ status: 'success' }])
  */
 // 💡 observable should be the core of js model. just like event target is the core of DOM
-export function createEventCenter<T extends EventConfig>(
-  whenAttach?: {
-    [P in keyof T as `${P & string}`]?: (utils: {
-      fn: (...params: Parameters<T[P]>) => void
-      emit: EventCenter<T>['emit']
-    }) => void
-  } & {
-    [P in keyof T as `${P & string}Initly`]?: (utils: {
-      fn: (...params: Parameters<T[P]>) => void
-      emit: EventCenter<T>['emit']
-    }) => void
-  }
-): EventCenter<T> {
+export function createEventCenter<T extends EventConfig>(whenAttach?: {
+  [P in keyof T as `${P & string}`]?: (utils: {
+    fn: (...params: Parameters<T[P]>) => void
+    emit: EventCenter<T>['emit']
+    isFirst: boolean
+  }) => void
+}): EventCenter<T> {
   const _eventCenterId = generateEventCenterId()
   const storedCallbackStore = new Map<keyof T, WeakerSet<AnyFn>>()
   type CallbackParam = any[]
@@ -91,8 +99,7 @@ export function createEventCenter<T extends EventConfig>(
     storedCallbackStore.set(eventName, callbackList)
 
     // handle `whenAttached` side-effect
-    whenAttach?.[eventName]?.({ fn, emit })
-    if (!storedCallbackStore.has(eventName)) whenAttach?.[`${eventName}Initly`]?.({ fn, emit })
+    whenAttach?.[eventName]?.({ fn, emit, isFirst: !storedCallbackStore.has(eventName) })
 
     // initly invoke prev emitedValues
     const cachedValues = emitedValueCache.get(eventName)
@@ -107,35 +114,45 @@ export function createEventCenter<T extends EventConfig>(
     })
   }
 
+  const createOnFactory = (eventName: string) => (fn: AnyFn, options?: EventCenterOptions) =>
+    singlyOn(eventName, fn, options)
+
+  const on = ((...args) => {
+    if (args.length === 1) {
+      return createOnFactory(...(args as [string]))
+    } else {
+      return singlyOn(...(args as [string, AnyFn, EventCenterOptions]))
+    }
+  }) as EventCenter<T>['on']
+
   const registEvents = ((subscriptionFns, options) =>
     map(
       subscriptionFns,
       (handlerFn, eventName) => handlerFn && singlyOn(String(eventName), handlerFn as AnyFn, options)
     )) as EventCenter<T>['registEvents']
 
-  const eventCenter = new Proxy(
-    { registEvents, on: singlyOn, emit, _eventCenterId },
-    {
-      get(target, p) {
-        if (target[p] !== undefined) return target[p]
-        if (String(p).startsWith('on'))
-          return (fn: AnyFn, options?: EventCenterOptions) => singlyOn(uncapitalize(String(p).slice(2)), fn, options)
-        return undefined
-      }
-    }
-  ) as unknown as EventCenter<T>
+  function clearAll() {
+    storedCallbackStore.clear()
+  }
+  function clear(eventName: keyof T) {
+    storedCallbackStore.delete(eventName)
+  }
+
+  const eventCenter = { registEvents, on, emit, clearAll, clear, _eventCenterId } as EventCenter<T>
+
   return eventCenter
 }
 
-export function mergeEventCenterFeature<O extends object, T extends EventConfig>(
-  targetObject: O,
-  eventCenter: EventCenter<T>
-): O & EventCenter<T> {
-  const merged = new Proxy(targetObject, {
-    get(target, p) {
-      if (p in targetObject) return target[p]
-      return eventCenter[p]
-    }
-  }) as O & EventCenter<T>
-  return merged
-}
+// export function mergeEventCenterFeature<O extends object, T extends EventConfig>(
+
+//   targetObject: O,
+//   eventCenter: EventCenter<T>
+// ): O & EventCenter<T> {
+//   const merged = new Proxy(targetObject, {
+//     get(target, p) {
+//       if (p in targetObject) return target[p]
+//       return eventCenter[p]
+//     }
+//   }) as O & EventCenter<T>
+//   return merged
+// }
