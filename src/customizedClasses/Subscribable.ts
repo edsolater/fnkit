bb2fe46e8767d4e8293802fee804afd2332ad7c8
edsoalter <edsolater@qq.com>
2023-10-13 10:44:22 +0800

    🚧 new value of subscribable

--- src/customizedClasses/Subscribable.ts
+++ src/customizedClasses/Subscribable.ts
@@ -1,7 +1,7 @@
 import { isFunction } from '../dataType'
 import { AnyFn } from '../typings'
 import { shrinkFn } from '../wrapper'
-export type SubscribeFn<T> = ((value: T) => void | Promise<void>) | ((newValue: T) => void)
+export type SubscribeFn<T> = ((value: T) => void | Promise<void>) | ((newValue: T, prevValue: T | undefined) => void)
 
 export type Subscribable<T> = {
   current: T
@@ -29,35 +29,44 @@ export function createSubscribable<T>(
 export function createSubscribable<T>(
   defaultValue?: T | (() => T),
   defaultCallbacks?: SubscribeFn<T>[]
-): [subscribable: Subscribable<T>, setValue: (dispatcher: SubscribableSetValueDispatcher<T | undefined>) => void] {
+): [
+  subscribable: Subscribable<T | undefined>,
+  setValue: (dispatcher: SubscribableSetValueDispatcher<T | undefined>) => void
+] {
   const callbacks = new Set<SubscribeFn<T>>(defaultCallbacks)
   const cleanFnMap = new Map<SubscribeFn<T>, AnyFn>()
 
   let innerValue = shrinkFn(defaultValue) as T
 
-  callbacks.forEach(invokeCallback)
+  callbacks.forEach((cb) => invokeCallback(cb, innerValue, undefined))
 
   function changeValue(dispatcher: SubscribableSetValueDispatcher<T | undefined>) {
     const newValue = isFunction(dispatcher) ? dispatcher(innerValue) : dispatcher
+    callbacks.forEach((cb) => invokeCallback(cb, newValue, innerValue))
     if (newValue != null) {
       innerValue = newValue
     }
-    callbacks.forEach(invokeCallback)
   }
 
-  function invokeCallback(cb: SubscribeFn<T>) {
+  function invokeCallback(cb: SubscribeFn<T>, newValue: T | undefined, prevValue: T | undefined) {
     const oldCleanFn = cleanFnMap.get(cb)
     if (isFunction(oldCleanFn)) oldCleanFn(innerValue)
-    const cleanFn = cb(innerValue)
+    const cleanFn = cb(newValue as T /*  type force */, prevValue)
     if (isFunction(cleanFn)) cleanFnMap.set(cb, cleanFn)
   }
 
   const subscribable = {
+    /** not clear for subscribable
+     * @deprecated use `subscribable.value()` instead
+     */
     get current() {
       return innerValue
     },
+    value() {
+      return innerValue
+    },
     subscribe(cb: any) {
-      if (innerValue != null) invokeCallback(cb) // immediately invoke callback, if has value
+      if (innerValue != null) invokeCallback(cb, innerValue, undefined) // immediately invoke callback, if has value
       callbacks.add(cb)
       return {
         unsubscribe() {
