2220bb96eef981c4dbe814b2bc9931b97c3aefe2
edsoalter <edsolater@qq.com>
2023-12-16 21:35:26 +0800

    ✨ omitItems

--- src/oldMethodsArray.ts
+++ src/oldMethodsArray.ts
@@ -245,3 +245,20 @@ export async function asyncReduce(
         Promise.resolve(initialValue)
       )
 }
+
+/** type C = OmitItems<'a' | 'b' | 'c', 'a' | 'b' | 'd'> // 'c' */
+export type OmitItem<
+  OriginString extends keyof any,
+  OmitString extends keyof any | undefined = undefined
+> = OriginString extends infer T ? (T extends OmitString ? never : T) : never
+
+/**
+ * Returns a new array with all elements of the input array except for the specified items.
+ * @param arr The input array to filter.
+ * @param items The item(s) to omit from the array.
+ * @returns A new array with all elements of the input array except for the specified items.
+ */
+export function omitItems<T>(arr: T[], items: T | T[]): T[] {
+  const omitSet = new Set(Array.isArray(items) ? items : [items])
+  return arr.filter((item) => !omitSet.has(item))
+}
