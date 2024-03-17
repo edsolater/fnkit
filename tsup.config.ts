bcd183dff0becd53b1de89923e5e98673bcf28eb
zhanggongying <edsolater@qq.com>
2024-03-17 22:11:24 +0800

    🐛 fix test error

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  entry: ["src/**/*.ts"],
+  entry: ["src/**/*.ts", "!src/**/*.test.ts"],
   clean: true,
   dts: true,
   format: ["esm"],
