e316a58498c9d339271f7b9d89d7adbb738e015b
edsolater <edsolater@qq.com>
2024-08-24 18:35:09 +0800

    🚧 richer basic forEach

--- src/collectionMethods/arrayEnhanceMethods.ts
+++ src/collectionMethods/arrayEnhanceMethods.ts
@@ -1,6 +1,7 @@
 import { GetCollectionKey, type Collection } from "."
 import { isArray, isMap, isSet } from "../dataType"
 
+/** for collection: object map */
 export function getKeys<T extends Collection>(...collection: readonly T[]): GetCollectionKey<T>[] {
   if (isMap(collection) || isArray(collection) || isSet(collection)) {
     // @ts-expect-error error for wrong type intelligense
--- src/collectionMethods/forEach.ts
+++ src/collectionMethods/forEach.ts
@@ -1,28 +1,39 @@
-import { AnyObj, isArray } from ".."
-import { AnyArr } from "../typings"
+import { AnyObj, isArray, isIterable, isMap, isObjectLike, isSet } from ".."
 
-export function forEach<T extends AnyArr>(
-  collection: T,
-  predicate: (item: T[number], index: number, arr: T) => void,
+/** the Method of map`filter`some etc. */
+export function forEach<T>(
+  collection: readonly T[],
+  predicate: (item: T, index: number, arr: readonly T[]) => void,
 ): void
+export function forEach<K>(collection: Set<K>, predicate: (item: K, itemSelfKey: K, original: Set<K>) => void): void
+export function forEach<K, V>(collection: Map<K, V>, predicate: (value: V, key: K, original: Map<K, V>) => void): void
 export function forEach<O extends AnyObj>(
   collection: O,
   predicate: (value: O[keyof O], key: keyof O, obj: O) => void,
 ): void
 export function forEach(collection, predicate) {
-  return isArray(collection) ? collection.forEach(predicate) : forEachEntry(collection, ([k, v]) => predicate(v, k))
-}
-
-export function forEachEntry<O extends AnyObj>(
-  collection: O,
-  predicate: (entry: [key: keyof O, value: O[keyof O]], obj: O) => void,
-): void {
-  Object.entries(collection).forEach(([k, v]) => predicate([k, v], collection))
+  if (!collection) return
+  if (isArray(collection)) {
+    return collection.forEach(predicate)
+  } else if (isSet(collection)) {
+    for (const k of collection) predicate(k, k, collection)
+  } else if (isMap(collection)) {
+    for (const [k, v] of collection) predicate(v, k, collection)
+  } else if (isIterable(collection)) {
+    for (const v of collection) {
+      if (isArray(v) && v.length === 2) predicate(v[1], v[0], collection)
+      if (isObjectLike(v) && "value" in v && "key" in v) predicate(v.value, v.key, collection)
+      if (v) predicate(v, v, collection)
+    }
+  } else {
+    Object.entries(collection).forEach(([k, v]) => predicate([k, v], collection))
+  }
 }
 
 export function forEachKey<O extends AnyObj>(
   collection: O,
   predicate: (key: keyof O, value: O[keyof O], obj: O) => void,
 ): void {
-  forEachEntry(collection, ([k, v], obj) => predicate(k, v, obj))
+  // @ts-ignore force for alias
+  return forEach(collection, (_, k, v) => predicate(k, v, collection))
 }
