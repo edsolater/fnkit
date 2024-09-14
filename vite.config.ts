a90cbe4cd1bb95b94c59577cdda45b77e577d49c
edsolater <edsolater@qq.com>
2024-09-14 14:17:33 +0800

    🛠️ use vite to build

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
+
+export default defineConfig({
+  plugins: [
+    dts({
+      tsconfigPath: "./tsconfig.json",
+      outDir: "dist",
+    }),
+  ],
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      formats: ["es"],
+      fileName: (format, entryName) => `${entryName}.js`,
+    },
+    rollupOptions: {
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: "src",
+        dir: "dist",
+      },
+    },
+    minify: false, // Disable overall compression
+    terserOptions: {
+      mangle: false, // Disable variable name mangling
+    },
+  },
+})
