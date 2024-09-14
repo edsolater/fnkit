41490468dd23f4ae753a280ac002cf389e074c12
edsolater <edsolater@qq.com>
2024-09-14 23:02:53 +0800

    🏷️ improve `mergeFunction`

--- src/mergeFunctions.ts
+++ src/mergeFunctions.ts
@@ -7,22 +7,35 @@ import { AnyFn } from "./typings/constants"
  * const multi = (a: number) => 3 + a
  * const c = mergeFunction(add, multi) // (a: number, b: number) => {add(a, b); multi(a, b)}
  */
-export function mergeFunction<T extends AnyFn>(...fns: [T]): (...params: Parameters<T>) => [ReturnType<T>]
-export function mergeFunction<T extends AnyFn, U extends (...args: Parameters<T>) => any>(
-  ...fns: [T, U]
-): (...params: Parameters<T>) => [ReturnType<T>, ReturnType<U>]
+export function mergeFunction<T extends AnyFn | undefined>(
+  ...fns: [T]
+): (...params: Parameters<T & AnyFn>) => [ReturnType<T & AnyFn>]
 export function mergeFunction<
-  T extends AnyFn,
-  U extends (...args: Parameters<T>) => any,
-  V extends (...args: Parameters<T>) => any,
->(...fns: [T, U, V]): (...params: Parameters<T>) => [ReturnType<T>, ReturnType<U>, ReturnType<V>]
+  T extends AnyFn | undefined,
+  U extends ((...args: Parameters<T & AnyFn>) => any) | undefined,
+>(...fns: [T, U]): (...params: Parameters<T & AnyFn>) => [ReturnType<T & AnyFn>, ReturnType<U & AnyFn>]
 export function mergeFunction<
-  T extends AnyFn,
-  U extends (...args: Parameters<T>) => any,
-  V extends (...args: Parameters<T>) => any,
-  W extends (...args: Parameters<T>) => any,
->(...fns: [T, U, V, W]): (...params: Parameters<T>) => [ReturnType<T>, ReturnType<U>, ReturnType<V>, ReturnType<W>]
-export function mergeFunction<T extends AnyFn>(...fns: T[]): (...params: Parameters<T>) => ReturnType<T>[]
-export function mergeFunction<T extends AnyFn>(...fns: T[]): (...params: Parameters<T>) => ReturnType<T>[] {
+  T extends AnyFn | undefined,
+  U extends ((...args: Parameters<T & AnyFn>) => any) | undefined,
+  V extends ((...args: Parameters<U & AnyFn>) => any) | undefined,
+>(
+  ...fns: [T, U, V]
+): (...params: Parameters<T & AnyFn>) => [ReturnType<T & AnyFn>, ReturnType<U & AnyFn>, ReturnType<V & AnyFn>]
+export function mergeFunction<
+  T extends AnyFn | undefined,
+  U extends ((...args: Parameters<T & AnyFn>) => any) | undefined,
+  V extends ((...args: Parameters<U & AnyFn>) => any) | undefined,
+  W extends ((...args: Parameters<V & AnyFn>) => any) | undefined,
+>(
+  ...fns: [T, U, V, W]
+): (
+  ...params: Parameters<T & AnyFn>
+) => [ReturnType<T & AnyFn>, ReturnType<U & AnyFn>, ReturnType<V & AnyFn>, ReturnType<W & AnyFn>]
+export function mergeFunction<T extends AnyFn | undefined>(
+  ...fns: T[]
+): (...params: Parameters<T & AnyFn>) => ReturnType<T & AnyFn>[]
+export function mergeFunction<T extends AnyFn | undefined>(
+  ...fns: T[]
+): (...params: Parameters<T & AnyFn>) => ReturnType<T & AnyFn>[] {
   return (...params) => fns.map((fn) => fn?.(...params))
 }
