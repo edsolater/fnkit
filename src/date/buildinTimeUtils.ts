c23fe2b08ac74f916e9f72987f654fda37817a75
edsolater <edsolater@gmail.com>
2024-10-23 22:45:45 +0800

    ✨ `setInterval` and `setTimeout`

--- src/date/buildinTimeUtils.ts
+++ src/date/buildinTimeUtils.ts
@@ -7,14 +7,58 @@ export type TimeSigal = number /* s */ | `${number}${"ms" | "s" | "m" | "h" | "d
  * build-in milliseconds is not human-friendly
  */
 export function setIntervalWithSecondes(fn: (...args: any[]) => void, interval?: TimeSigal | undefined) {
-  return setInterval(fn, interval ? parseTimeSignal(interval) : undefined)
+  return globalThis.setInterval(fn, interval ? parseTimeSignal(interval) : undefined)
+}
+
+/**
+ * build-in globalThis.setInterval is not human-friendly
+ * @param fn function to run
+ * @param options
+ * @returns
+ */
+export function setInterval(
+  fn: (...args: any[]) => void,
+  options?: {
+    interval: TimeSigal
+    runImmediate?: boolean
+  },
+): { cancel(): void } {
+  if (options?.runImmediate) fn()
+  const timeId = setIntervalWithSecondes(fn, options?.interval)
+  return {
+    cancel() {
+      clearInterval(timeId)
+    },
+  }
 }
 
 /**
  * build-in milliseconds is not human-friendly
  */
 export function setTimeoutWithSecondes(fn: (...args: any[]) => void, delay?: TimeSigal | undefined) {
-  return setTimeout(fn, delay ? parseTimeSignal(delay) : undefined)
+  return globalThis.setTimeout(fn, delay ? parseTimeSignal(delay) : undefined)
+}
+
+/**
+ * build-in globalThis.setTimeout is not human-friendly
+ * @param fn function to run
+ * @param options
+ * @returns
+ */
+export function setTimeout(
+  fn: (...args: any[]) => void,
+  options?: {
+    delay: TimeSigal
+    runImmediate?: boolean
+  },
+): { cancel(): void } {
+  if (options?.runImmediate) fn()
+  const timeId = setTimeoutWithSecondes(fn, options?.delay)
+  return {
+    cancel() {
+      clearTimeout(timeId)
+    },
+  }
 }
 
 /** to milliseconds */
