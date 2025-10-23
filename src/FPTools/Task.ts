59609b1688a5c965b368ab3919a208a3a5518ac1
edsolater <edsolater@gmail.com>
2025-10-24 03:45:23 +0800

    🩹 Task

--- src/FPTools/Task.ts
+++ src/FPTools/Task.ts
@@ -1,13 +1,16 @@
-import type { AnyFn } from "../typings"
+import { isFunction } from "../dataType"
+import type { AnyFn, MayFn } from "../typings"
 
 type TaskResult<T> = T | Promise<T>
 type Exec<T, U = any> = (pastTaskValue: U) => TaskResult<T>
 type Taskable<T, U = any> = TaskResult<T> | Task<T> | Exec<T, U>
 
 type InnerTaskState<T> = {
-  is: "pending" | "fulfilled" | "rejected" | "running"
+  is: "pending" | "fulfilled" | "rejected" | "running" | "aborted" /* 冻结状态，不可再执行 */
   value?: T
   error?: any
+  /** 由 catch 注入 */
+  fallback?: (error: any) => T | Promise<T>
   taskRunMs?: number
   parentTasks: Task<any>[] // 尚未执行的任务（基于此状态派生，供状态变更时通知更新）// 只有上一个任务执行完毕且成功了才可以进行下一个任务。
   registeredCallbacks: {
@@ -52,30 +55,57 @@ export class Task<T, U = any> {
   }
 
   /** 真正启动过程，返回一个可等待的 Promise */
-  private async executeThisTask(payload: { prevValue: U }): Promise<T> {
-    const taskStartTime = globalThis.Performance?.now?.()
-    try {
-      const result = await this.execFn(payload.prevValue)
-      this.taskState.is = "fulfilled"
-      this.taskState.value = result
-      Promise.resolve().then(() => {
-        this.taskState.registeredCallbacks.onFulfilled?.forEach((fn) => fn?.(result))
-        this.taskState.registeredCallbacks.onFinally?.forEach((fn) => fn?.())
-      })
-      const taskEndTime = globalThis.Performance?.now?.()
-      this.taskState.taskRunMs = taskEndTime - taskStartTime
-      return result
-    } catch (e) {
-      this.taskState.is = "rejected"
-      this.taskState.error = e
-      Promise.resolve().then(() => {
-        this.taskState.registeredCallbacks.onRejected?.forEach((fn) => fn?.(e))
-        this.taskState.registeredCallbacks.onFinally?.forEach((fn) => fn?.())
-      })
-      const taskEndTime = globalThis.Performance?.now?.()
-      this.taskState.taskRunMs = taskEndTime - taskStartTime
-      return Promise.reject(e)
+  private async runCurrentTask(payload: { prevValue: U }): Promise<T> {
+    // 以标注为取消的任务不可再执行
+    if (this.taskState.is === "aborted") return Promise.reject(this.taskState.error)
+
+    this.taskState.is = "running"
+    const {
+      promise: CurrentTaskResultPromise,
+      resolve: CurrentTaskResultResolve,
+      reject: CurrentTaskResultReject,
+    } = Promise.withResolvers<T>()
+    const finalCore = (payload: { prevValue: U }) => {
+      const taskStartTime = globalThis.Performance?.now?.()
+      const { promise: costTimePromise, resolve: costTimeResolve } = Promise.withResolvers<number>()
+      const result = Promise.resolve()
+        .then(() => this.execFn(payload.prevValue))
+        .catch((e) => {
+          if (this.taskState.fallback) {
+            return this.taskState.fallback(e)
+          } else {
+            return Promise.reject(e)
+          }
+        })
+        .finally(() => {
+          costTimeResolve(globalThis.Performance?.now?.() - taskStartTime)
+        })
+
+      return { result, costTime: costTimePromise }
     }
+    const { result, costTime } = finalCore({ prevValue: payload.prevValue })
+    Promise.allSettled([result, costTime]).then(([resResult, resCostTime]) => {
+      this.taskState.taskRunMs = resCostTime.status === "fulfilled" ? resCostTime.value : NaN
+      if (resResult.status === "fulfilled") {
+        this.taskState.is = "fulfilled"
+        this.taskState.value = resResult.value
+        Promise.resolve().then(() => {
+          this.taskState.registeredCallbacks.onFulfilled?.forEach((fn) => fn?.(resResult.value))
+          this.taskState.registeredCallbacks.onFinally?.forEach((fn) => fn?.())
+        })
+        CurrentTaskResultResolve(resResult.value)
+      } else {
+        this.taskState.is = "rejected"
+        this.taskState.error = resResult.reason
+        Promise.resolve().then(() => {
+          this.taskState.registeredCallbacks.onRejected?.forEach((fn) => fn?.(resResult.reason))
+          this.taskState.registeredCallbacks.onFinally?.forEach((fn) => fn?.())
+        })
+        CurrentTaskResultReject(resResult.reason)
+      }
+    })
+
+    return CurrentTaskResultPromise
   }
 
   // /** 假运行：不启动计算，仅返回一个 Promise（用于链式衔接） */
@@ -89,7 +119,7 @@ export class Task<T, U = any> {
   async run(): Promise<T> {
     let taskResult: any = undefined
     for (const parentTask of this.taskState.parentTasks.concat(this)) {
-      taskResult = await parentTask.executeThisTask({ prevValue: taskResult })
+      taskResult = await parentTask.runCurrentTask({ prevValue: taskResult })
     }
     return taskResult
   }
@@ -121,6 +151,23 @@ export class Task<T, U = any> {
     const newTask = Task.from<V, T>(taskable)
     newTask.taskState.parentTasks = this.taskState.parentTasks.concat(this)
     return newTask
+  }
+
+  private abortCurrentTask(): void {
+    this.taskState.is = "aborted"
+    this.taskState.error = new Error("This task has been aborted and cannot be executed.")
+  }
+
+  /** 中止任务（不可再执行） */
+  abort(): void {
+    this.abortCurrentTask()
+    this.taskState.parentTasks.toReversed().forEach((task) => task.abortCurrentTask())
+  }
+
+  /** 如果任务失败， 捕获错误并返回默认值 */
+  catch(defaultValue: T | Promise<T> | ((error: any) => T | Promise<T>)): Task<T, U> {
+    this.taskState.fallback = isFunction(defaultValue) ? defaultValue : () => defaultValue
+    return this
   }
 }
 
