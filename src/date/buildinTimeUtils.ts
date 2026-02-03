ba3d7533974e9375a945880da0130e0e31b7b4bd
edsolater <edsolater@gmail.com>
2026-02-03 23:45:54 +0800

    🚧 强化 setInterval

--- src/date/buildinTimeUtils.ts
+++ src/date/buildinTimeUtils.ts
@@ -1,5 +1,5 @@
-import { shrinkFn, type MayFn } from ".."
-import { isNumber, isString } from "../dataType"
+import { assert, getValue, shrinkFn, type MayFn } from ".."
+import { isNumber, isObject, isString, isUndefined } from "../dataType"
 import { asyncInvoke } from "../functionManagers"
 
 /** use seconds not milliseconds */
@@ -51,15 +51,36 @@ export type IntervalTaskFunction = (utils: {
   loopIndex: number
   changeInterval: (newInterval: MayFn<TimeType, [oldIntervalSeconds: number]>) => void
   forceRunNextLoop: () => void
-}) => void
+}) => void | Promise<void> | any | Promise<any>
 
 export type SetIntervalOptions = {
   /** if you want run immediately after delay. both set `delay` and `immediate` */
   delay?: TimeType
   interval?: TimeType
   immediate?: boolean
-  /** if set this, don't auto-run  */
+  /** if set this, don't auto-run，相反，控制权交给返回的 Controller  */
   haveManuallyController?: boolean
+
+  /**
+   * TODO:还未实现
+   * 当就任务未运行完成，新任务到达。
+   * 此时如何面对新触发的任务。
+   *
+   * invoke-income: 默认。即使冲突了，也不做任何处理，直接触发触新任务
+   * skip-income: 如果上一个任务未完成，直接跳过新的任务
+   * queue-income: 如果上一个任务未完成，则将新的任务加入排队等待
+   * cancel-prev-and-invoke-income: 若上一个任务未完成，则取消旧的任务，立刻运行新的任务
+   *
+   * or (prevTaskInvokeController) => any: 自定义冲突处理函数，
+   * 接收上一个任务的控制器 (由AsyncInvoke给出，智能给出cancel的指示， 但是否能停下来看内部有没有打断点) 作为参数，可以通过它来取消上一个任务等操作
+   *
+   */
+  whenTwoTaskConflict?:
+    | "invoke-income"
+    | "skip-income"
+    | "queue-income"
+    | "cancel-prev-and-invoke-income"
+    | ((prevTaskInvokeController: any) => any)
 }
 
 export type SetIntervalController = {
@@ -68,52 +89,126 @@ export type SetIntervalController = {
   forceRunNextLoop(): void
 }
 
+export type SetIntervalVerboseOptions = SetIntervalOptions | TimeType
 /**
  * build-in globalThis.setInterval is not human-friendly
  * @param taskFn function to run (run in future, event immediately, it will run in  micro task)
  * @param options
  * @returns
  */
-export function setInterval(taskFn: IntervalTaskFunction, _options?: SetIntervalOptions | TimeType): SetIntervalController {
+export function setInterval(
+  taskFn: IntervalTaskFunction,
+  verboseOption?: SetIntervalVerboseOptions,
+): SetIntervalController {
+  // --- 内部状态 ---
   let loopIndex = 0
   let intervalTimeId = 0
-  let timeoutId = 0
-  const options: SetIntervalOptions | undefined = isTimeType(_options) ? { interval: _options } : _options
-  let intervalSeconds = parseTimeTypeToSeconds(options?.interval ?? 1)
+  let initDelayTimeoutId = 0
+  const taskRecorder: Map<
+    string | number,
+    {
+      resultPromise: Promise<any>
+      hasAborted: boolean // 该loop的任务是否被中止
+    }
+  > = new Map()
 
-  function changeInterval(newInterval: MayFn<TimeType, [oldIntervalSeconds: number]>) {
+  // --- 配置参数 ---
+  const options = {
+    ...(isObject(verboseOption) ? verboseOption : {}),
+    interval: parseTimeTypeToSeconds(
+      isUndefined(verboseOption) ? 1 : isTimeType(verboseOption) ? verboseOption : (verboseOption.interval ?? 1),
+    ),
+    whenTwoTaskConflict: getValue(verboseOption, "whenTwoTaskConflict", "invoke-income"),
+  }
+  let intervalSeconds = options.interval
+
+  function changeIntervalDuration(newInterval: MayFn<TimeType, [oldIntervalSeconds: number]>) {
     intervalSeconds = parseTimeTypeToSeconds(shrinkFn(newInterval, [intervalSeconds]))
     stopLoop()
     runLoop({ canWithImmediate: false })
   }
 
   function stopLoop() {
-    clearTimeout(timeoutId)
+    clearTimeout(initDelayTimeoutId) // 初始触发前的设定的Delay
     clearInterval(intervalTimeId)
   }
 
+  // 开启任务循环
   function runLoop(innerOptions: { canWithImmediate: boolean; forceImmediate?: boolean } = { canWithImmediate: true }) {
-    function runCore() {
-      asyncInvoke(() => taskFn({ loopIndex: loopIndex++, cancel: stopLoop, changeInterval, forceRunNextLoop: forceNext }))
-    }
-    function innerRun() {
-      if ((innerOptions.canWithImmediate && options?.immediate) || innerOptions.forceImmediate) runCore()
-      intervalTimeId = setIntervalWithSecondes(runCore, intervalSeconds)
+    const thisLoopIndex = loopIndex++
+
+    // 运转每个单个循环内的任务
+    async function execInputedTask() {
+      // isTaskRunning就是冲突符号
+      const hasRunningTask =
+        taskRecorder.size > 0 && taskRecorder.values().some((record) => record.hasAborted === false)
+      if (hasRunningTask) {
+        // 进入冲突时的逻辑
+        const whenConflict = options.whenTwoTaskConflict
+        // 等待上一个任务完成后再执行本次任务
+        const pastTaskId = thisLoopIndex - 1
+        const prevTaskResultPromise = taskRecorder.get(pastTaskId)?.resultPromise
+        assert(prevTaskResultPromise, "Thus isTaskRunning is true but no previous task found. this should not happen. ")
+
+        if (whenConflict === "skip-income") {
+          return prevTaskResultPromise
+        } else if (whenConflict === "queue-income") {
+          return prevTaskResultPromise.then(() => execInputedTask())
+        } else if (whenConflict === "cancel-prev-and-invoke-income") {
+          // TODO 还未写这个的逻辑，待继续
+          // 取消上一个任务
+          const pastTaskRecord = taskRecorder.get(pastTaskId)
+          if (pastTaskRecord) {
+            pastTaskRecord.hasAborted = true
+          }
+          return execInputedTask()
+        }
+      } else {
+        // 进入未冲突时的逻辑
+        const taskId = thisLoopIndex // 生成一个用于标识当前任务的ID
+        const resultPromise = Promise.resolve(
+          taskFn({
+            loopIndex: thisLoopIndex,
+            cancel: stopLoop,
+            changeInterval: changeIntervalDuration,
+            forceRunNextLoop: forceNext,
+          }),
+        ).finally(() => {
+          taskRecorder.delete(taskId)
+        })
+        taskRecorder.set(taskId, { hasAborted: false, resultPromise }) // 记录当前任务状态
+        return resultPromise
+      }
     }
 
-    if (!innerOptions.forceImmediate && options?.delay) {
-      timeoutId = setTimeoutWithSecondes(() => {
-        innerRun()
-      }, options.delay)
-    } else {
-      innerRun()
+    /** 任务循环管理器 */
+    function manageIntervalLoop(eachLoopFn: () => void) {
+      /** 开启任务循环 */
+      function startIntervalLoop() {
+        if ((innerOptions.canWithImmediate && options?.immediate) || innerOptions.forceImmediate) eachLoopFn()
+        intervalTimeId = setIntervalWithSecondes(eachLoopFn, intervalSeconds)
+      }
+  
+      // 直接触发/延迟触发
+      if (innerOptions.forceImmediate || options?.delay) {
+        startIntervalLoop()
+      } else {
+        initDelayTimeoutId = setTimeoutWithSecondes(() => {
+          startIntervalLoop()
+        }, options.delay)
+      }
     }
+
+    manageIntervalLoop(execInputedTask)
   }
+
+  // 强行触发下一次循环
   function forceNext() {
     stopLoop()
     runLoop({ canWithImmediate: true, forceImmediate: true })
   }
 
+  // 立刻开始倒计时
   if (!options?.haveManuallyController) {
     runLoop()
   }
@@ -150,14 +245,11 @@ export type SetTimeoutController = {
  * @param options
  * @returns
  */
-export function setTimeout(
-  taskFn: TimeoutTaskFunction,
-  _options?: SetTimeoutOptions | TimeType,
-): SetTimeoutController {
+export function setTimeout(taskFn: TimeoutTaskFunction, _options?: SetTimeoutOptions | TimeType): SetTimeoutController {
   let loopCount = 0
   let timeId = 0
 
-  const options: SetTimeoutOptions = isTimeType(_options) ? { delay: _options } : _options ?? {}
+  const options: SetTimeoutOptions = isTimeType(_options) ? { delay: _options } : (_options ?? {})
   // core
   const runCore = () => asyncInvoke(() => taskFn({ loopCount: loopCount++, cancel }))
 
